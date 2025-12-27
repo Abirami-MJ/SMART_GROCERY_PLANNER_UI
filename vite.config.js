eb5929ff30f41233445e4b090dc7df18c257ddac
AbiramimeiyappanMJ <abi2209mj@gmail.com>
2025-12-27 19:31:44 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/SMART_GLOCERY_PLANNER_UI/',
   plugins: [react()],
   server: {
     port: 3000,
