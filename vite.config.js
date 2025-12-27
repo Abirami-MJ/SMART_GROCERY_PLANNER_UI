7fe8f15715361f92df65646a8740aa3cd62ee6e4
AbiramimeiyappanMJ <abi2209mj@gmail.com>
2025-12-27 19:54:56 +0530

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-refresh'
 
 export default defineConfig({
   base: '/SMART_GLOCERY_PLANNER_UI/',
